7c4ed2dc39760230166f6e36d526aee544891233
nanthakumaran-s <nanthankumaransenthil@gmail.com>
2023-05-24 12:50:10 +0530

    docs: added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,31 @@ const rightArr = document.getElementById("right");
 const sections = document.getElementsByTagName("section");
 const input = document.getElementsByTagName("input");
 
+// Adding event listeners
 for (let i = 0; i < input.length; i++) {
   input[i].addEventListener("change", (e) => changeColor(e));
 }
 
-let currentSectionInView = 0;
-
 leftArr?.addEventListener("click", () => changeSection(-1, 0));
 rightArr?.addEventListener("click", () => changeSection(1, 0));
 
+// State for current section
+let currentSectionInView = 0;
+
 const changeSection = (action, index) => {
+  // Perform change
   if (action === 0) {
     currentSectionInView = index;
   } else {
     currentSectionInView = currentSectionInView + action;
   }
 
+  // animate into view
   sections[currentSectionInView].scrollIntoView({
     behavior: "smooth",
   });
 
+  // Update left and right arrow visibility
   if (currentSectionInView === 0) {
     leftArr?.classList.add("hide");
   } else {
@@ -37,6 +42,7 @@ const changeSection = (action, index) => {
   }
 };
 
+// Change color
 const changeColor = (event) => {
   sections[currentSectionInView].style.backgroundColor = event.target.value;
 };
